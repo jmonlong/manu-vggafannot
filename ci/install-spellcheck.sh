558b39183b10a6ef6ffc101c7d514597c40b8068
Jean Monlong <jean.monlong@gmail.com>
2025-06-13 17:30:31 +0200

    trying to fix CI

--- ci/install-spellcheck.sh
+++ ci/install-spellcheck.sh
@@ -6,7 +6,7 @@
 set -o errexit \
     -o pipefail
 
-sudo apt-get update --yes
+sudo apt-get update --yes --allow-releaseinfo-change
 sudo apt-get install --yes aspell aspell-en
 wget --directory-prefix=build/pandoc/filters \
   https://github.com/pandoc/lua-filters/raw/13c3fa7e97206413609a48a82575cb43137e037f/spellcheck/spellcheck.lua
